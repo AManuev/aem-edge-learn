e940061bffbeccb6d6111b6dcd3b5313e668fbd4
SAP\Andrii.Manuiev <andrii.manuiev@sap.com>
2024-07-10 15:23:08 +0300

    rename files.

---
+++ blocks/hero-columns/hero-columns.js
@@ -0,0 +1,11 @@
+import { getMetadata } from '../../scripts/aem.js';
+
+/**
+ * loads and decorates the hero
+ * @param {Element} block The hero column
+ */
+export default async function decorate(block) {
+
+  const heroColumnsMeta = getMetadata('hero_columns');
+  console.log(heroColumnsMeta);
+}
