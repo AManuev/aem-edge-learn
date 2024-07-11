c97e37936e47b3acee79383c7ee4c8b1663fc8c3
SAP\Andrii.Manuiev <andrii.manuiev@sap.com>
2024-07-11 12:58:20 +0300

    Add new component. Add background color logic

--- blocks/hero-columns/hero-columns.js
+++ blocks/hero-columns/hero-columns.js
@@ -1,6 +1,10 @@
 export default function decorate(block) {
   const firstLink = block.querySelector('a');
-
   firstLink.style.color = 'blue';
   firstLink.style.backgroundColor = 'white';
+
+  const bkColor = block.children[0].firstElementChild.firstElementChild.firstElementChild.alt.trim();
+  if (bkColor) {
+    block.parentElement.style.backgroundColor = bkColor;
+  }
 }
