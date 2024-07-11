e7e2dcefd3ace62259427268bbf3e8d7575a4ae4
SAP\Andrii.Manuiev <andrii.manuiev@sap.com>
2024-07-11 15:08:58 +0300

    Add JS

--- blocks/hero-columns/hero-columns.js
+++ blocks/hero-columns/hero-columns.js
@@ -7,6 +7,17 @@ export default function decorate(block) {
       const bkColor = block.children[2].firstElementChild.firstElementChild.innerText.trim()
 
       if (bkColor) {
+        if (block.classList.contains('white') || block.classList.contains('magenta') || block.classList.contains('black')) {
+            block.classList.remove('white');
+            block.classList.remove('magenta');
+            block.classList.remove('black');
+        }
+        if (block.parentElement.classList.contains('white') || block.parentElement.classList.contains('magenta') || block.parentElement.classList.contains('black')) {
+            block.parentElement.classList.remove('white');
+            block.parentElement.classList.remove('magenta');
+            block.parentElement.classList.remove('black');
+        }
+
         block.classList.add(bkColor.toLowerCase());
         block.parentElement.classList.add(bkColor.toLowerCase());
       }
