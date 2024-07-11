9e9c1388fce7f390d0a745f9f58b3d2d8820bdc3
SAP\Andrii.Manuiev <andrii.manuiev@sap.com>
2024-07-11 15:11:19 +0300

    Fix spaces

--- blocks/hero-columns/hero-columns.js
+++ blocks/hero-columns/hero-columns.js
@@ -3,24 +3,24 @@ export default function decorate(block) {
   firstLink.style.color = 'blue';
   firstLink.style.backgroundColor = 'white';
 
- if (block.children[2]) {
-      const bkColor = block.children[2].firstElementChild.firstElementChild.innerText.trim()
+  if (block.children[2]) {
+    const bkColor = block.children[2].firstElementChild.firstElementChild.innerText.trim()
 
-      if (bkColor) {
-        if (block.classList.contains('white') || block.classList.contains('magenta') || block.classList.contains('black')) {
-            block.classList.remove('white');
-            block.classList.remove('magenta');
-            block.classList.remove('black');
-        }
-        if (block.parentElement.classList.contains('white') || block.parentElement.classList.contains('magenta') || block.parentElement.classList.contains('black')) {
-            block.parentElement.classList.remove('white');
-            block.parentElement.classList.remove('magenta');
-            block.parentElement.classList.remove('black');
-        }
-
-        block.classList.add(bkColor.toLowerCase());
-        block.parentElement.classList.add(bkColor.toLowerCase());
+    if (bkColor) {
+      if (block.classList.contains('white') || block.classList.contains('magenta') || block.classList.contains('black')) {
+        block.classList.remove('white');
+        block.classList.remove('magenta');
+        block.classList.remove('black');
       }
-      block.children[2].remove();
-  }
+      if (block.parentElement.classList.contains('white') || block.parentElement.classList.contains('magenta') || block.parentElement.classList.contains('black')) {
+        block.parentElement.classList.remove('white');
+        block.parentElement.classList.remove('magenta');
+        block.parentElement.classList.remove('black');
+      }
+
+      block.classList.add(bkColor.toLowerCase());
+      block.parentElement.classList.add(bkColor.toLowerCase());
+    }
+    block.children[2].remove();
+   }
 }
